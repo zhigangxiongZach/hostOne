6e240097c3369f7024c8223fc964d9b45c419759
zxiong1@yahoo.com <zxiong1@yahoo.com>
2021-08-31 01:30:43 -0400

    resize jpg files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    function renderProducts() {
+        const products = [
+            'IMG_9062.jpg',
+            'IMG_9063.jpg',
+            'IMG_9067.jpg',
+            'IMG_9068.jpg',
+            'IMG_9069.jpg',
+            'IMG_9070.jpg',
+            'IMG_9071.jpg',
+            'IMG_9072.jpg',
+            'IMG_9073.jpg'
+        ];
+
+        const list = []
+
+        for (const [i, product] of products.entries()) {
+            list.push(
+                <li>{i + 1}
+                    <span> - </span>
+                    <span>
+                        <a href={'image/'+product}>
+                            <img src={'image/'+product} height={window.innerHeight / 10}/>
+                        </a>
+                    </span>
+                </li>);
+        }
+
+        return (
+            <div>
+                <div>
+                    <span><h2>Welcome to Xiong's Family Picture site:</h2></span>
+                </div>
+                {list}
+            </div>
+        )
+    }
+
+    return renderProducts();
 }
 
 export default App;
