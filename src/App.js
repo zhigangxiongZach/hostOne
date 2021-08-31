ea121b9dbd4c032ff230a93654499a2cd294a298
zxiong1@yahoo.com <zxiong1@yahoo.com>
2021-08-31 09:46:58 -0400

    reformat

--- src/App.js
+++ src/App.js
@@ -5,32 +5,29 @@ function App() {
     function renderProducts() {
         const products = [
             'IMG_6625.jpg', 'IMG_6628.jpg', 'IMG_6631.jpg', 'IMG_6634.jpg', 'IMG_6637.jpg', 'IMG_6640.jpg',
-            'IMG_6643.jpg', 'IMG_9063.JPG', 'IMG_9069.JPG', 'IMG_9072.JPG', 'IMG_6626.jpg', 'IMG_6629.jpg',
-            'IMG_6632.jpg', 'IMG_6635.jpg', 'IMG_6638.jpg', 'IMG_6641.jpg', 'IMG_6644.jpg', 'IMG_9067.JPG',
-            'IMG_9070.JPG', 'IMG_9073.JPG', 'IMG_6627.jpg', 'IMG_6630.jpg', 'IMG_6633.jpg', 'IMG_6636.jpg',
-            'IMG_6639.jpg', 'IMG_6642.jpg', 'IMG_9062.JPG', 'IMG_9068.JPG', 'IMG_9071.JPG'
-
+            'IMG_6643.jpg', 'IMG_6626.jpg', 'IMG_6629.jpg', 'IMG_6632.jpg', 'IMG_6635.jpg', 'IMG_6638.jpg',
+            'IMG_6641.jpg', 'IMG_6644.jpg', 'IMG_6627.jpg', 'IMG_6630.jpg', 'IMG_6633.jpg', 'IMG_6636.jpg',
+            'IMG_6639.jpg', 'IMG_6642.jpg',
+            'IMG_9067.JPG', 'IMG_9070.JPG', 'IMG_9073.JPG', 'IMG_9062.JPG', 'IMG_9068.JPG', 'IMG_9071.JPG',
+            'IMG_9063.JPG', 'IMG_9069.JPG', 'IMG_9072.JPG'
         ];
 
         const list = []
 
         for (const [i, product] of products.entries()) {
             list.push(
-                <table>
-                    <td>
-
-                        <li>{i + 1}
-                            <span> - </span>
-                            <span>
+                <li>{i + 1}
+                    <span> - </span>
+                    <span>
                         <a href={'image/' + product}>
-                            <img src={'image/' + product} height={window.innerHeight / 10}/>
+                            <img src={'image/' + product} height={window.innerHeight / 5}/>
                         </a>
                     </span>
-                        </li>
-                    </td>
-                </table>
+                </li>
             );
         }
+        ;
+        const container = [];
 
         return (
             <div>
