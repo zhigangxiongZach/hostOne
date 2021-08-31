caf1eb2438b867f24a18e0fb3d77970b8a31bf99
zxiong1@yahoo.com <zxiong1@yahoo.com>
2021-08-31 09:18:12 -0400

    add more image to website

--- src/App.js
+++ src/App.js
@@ -4,16 +4,12 @@ function App() {
 
     function renderProducts() {
         const products = [
-            'IMG_6629.jpg',
-            'IMG_9062.jpg',
-            'IMG_9063.jpg',
-            'IMG_9067.jpg',
-            'IMG_9068.jpg',
-            'IMG_9069.jpg',
-            'IMG_9070.jpg',
-            'IMG_9071.jpg',
-            'IMG_9072.jpg',
-            'IMG_9073.jpg'
+            'IMG_6625.jpg', 'IMG_6628.jpg', 'IMG_6631.jpg', 'IMG_6634.jpg', 'IMG_6637.jpg', 'IMG_6640.jpg',
+            'IMG_6643.jpg', 'IMG_9063.JPG', 'IMG_9069.JPG', 'IMG_9072.JPG', 'IMG_6626.jpg', 'IMG_6629.jpg',
+            'IMG_6632.jpg', 'IMG_6635.jpg', 'IMG_6638.jpg', 'IMG_6641.jpg', 'IMG_6644.jpg', 'IMG_9067.JPG',
+            'IMG_9070.JPG', 'IMG_9073.JPG', 'IMG_6627.jpg', 'IMG_6630.jpg', 'IMG_6633.jpg', 'IMG_6636.jpg',
+            'IMG_6639.jpg', 'IMG_6642.jpg', 'IMG_9062.JPG', 'IMG_9068.JPG', 'IMG_9071.JPG'
+
         ];
 
         const list = []
@@ -23,17 +19,17 @@ function App() {
                 <table>
                     <td>
 
-                <li>{i + 1}
-                    <span> - </span>
-                    <span>
-                        <a href={'image/'+product}>
-                            <img src={'image/'+product} height={window.innerHeight / 10}/>
+                        <li>{i + 1}
+                            <span> - </span>
+                            <span>
+                        <a href={'image/' + product}>
+                            <img src={'image/' + product} height={window.innerHeight / 10}/>
                         </a>
                     </span>
-                </li>
+                        </li>
                     </td>
                 </table>
-                    );
+            );
         }
 
         return (
